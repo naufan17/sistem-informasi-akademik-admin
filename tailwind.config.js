3f40b9d96c6076d60f9cf6c3e933a4182a9c79ac
naufan17 <muhammadnaufan719@yahoo.com>
2022-08-02 16:14:12 +0700

    fix error tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,7 @@ const defaultTheme = require('tailwindcss/defaultTheme');
 module.exports = {
     content: [
         "./resources/views/**/*.blade.php",
+        "./resources/views/*.blade.php",
         "./resources/views/**/*.js",
         "./resources/views/**/*.vue",
       ],
